4af081680f091a5cb89f2d14b0e022b6d3de2eb1
LouisWright <louis--wright@hotmail.co.uk>
2022-12-09 13:57:34 +0000

    added the get random card button functionality

--- cards/src/components/getRanCard/getRanCard.js
+++ cards/src/components/getRanCard/getRanCard.js
@@ -1,23 +1,43 @@
 import "./randomcard.css";
 import useFetch from "../../hooks/useFetch";
+import { useState } from "react";
 
 function RandomCard() {
-  const { data, error } = useFetch(
-    "https://card-game-backend.onrender.com/api/cards/1"
-  );
+  const [cardNumber, setCardNumber] = useState(1);
+  const url =
+    cardNumber !== ""
+      ? `https://card-game-l81f.onrender.com/api/cards/${cardNumber}`
+      : undefined;
+  const { data, error } = useFetch(url);
+  console.log(data);
+
+  //   const cardValue = data.payload[0].card_value;
+  //   const cardSuit = data.payload[0].suit;
+  //   const cardImg = data.payload[0].img_url;
+
+  const cardInfo = {
+    value: data.payload[0].card_value,
+    suit: data.payload[0].suit,
+    img: data.payload[0].img_url,
+  };
 
   if (error) {
     return <p>error</p>;
   }
 
-  if (null === data) {
-    return <p>no card yet....fetching</p>;
-  }
+  //function HandleClick
 
   return (
     <>
-      <p className="card">{data}</p>
-      <button className="random-button">Click to get a random card</button>
+      <p className="card">{cardInfo.value + cardInfo.suit}</p>
+      <img className="card-img" src={cardInfo.img} alt="card-img"></img>
+      <button
+        className="random-button"
+        onClick={(e) => setCardNumber(Math.floor(Math.random() * 52))}
+        value={cardNumber}
+      >
+        Click to get a random card
+      </button>
     </>
   );
 }
